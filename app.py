057c9a07e68e3d1a4575b3175186cdffbdb0c0f6
serenabridget <157680977+serenabridget@users.noreply.github.com>
2024-04-04 16:24:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask,request,render_template
 import google.generativeai as palm
+import replicate
+import os
 
+os.environ["REPLICATE_API_TOKEN"] = "r8_YXLbAcu8KDC22YpGHEG4fQqMOgfcZSZ211o4L"
 palm.configure(api_key="AIzaSyCcxCravwb4gkDWcLDKW3kgj00Z-9JyLWI")
 model = {
     "model": "models/chat-bison-001"
@@ -23,16 +26,54 @@ def main():
         flag = 0
     return(render_template("main.html",r=name))
 
-@app.route("/text",methods=["GET","POST"])
-def text():
-    return(render_template("text.html"))
+@app.route("/image",methods=["GET","POST"])
+def image():
+    return(render_template("image.html"))
 
-@app.route("/text_generator",methods=["GET","POST"])
-def text_generator():
+@app.route("/image_generator",methods=["GET","POST"])
+def image_generator():
     q = request.form.get('q')
-    r = palm.chat(**model, messages =q)
-    return(render_template("text_generator.html",r=r.last))
+    r = replicate.run(
+    "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+    input = {
+        "prompt" : q
+    }
+)
+    return(render_template("image_generator.html",r=r[0]))
            
+@app.route("/music",methods=["GET","POST"])
+def music():
+    return(render_template("music.html"))
+
+@app.route("/music_generator",methods=["GET","POST"])
+def music_generator():
+    q = request.form.get('q')
+    r = replicate.run(
+    "meta/musicgen:7be0f12c54a8d033a0fbd14418c9af98962da9a86f5ff7811f9b3423a1f0b7d7",
+    input = {
+        "prompt" : q,
+        "duration" : 5
+    }
+)
+    return(render_template("music_generator.html",r=r))
+
+@app.route("/video",methods=["GET","POST"])
+def video():
+    return(render_template("video.html"))
+
+@app.route("/video_generator",methods=["GET","POST"])
+def video_generator():
+    q = request.form.get('q')
+    r = replicate.run(
+    "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+    input = {
+        "prompt" : q,
+        "num_frames" : 10
+    }
+)
+    return(render_template("video_generator.html",r=r[0]))
+
+
 @app.route("/end",methods=["GET","POST"])
 def end():
     global flag 
